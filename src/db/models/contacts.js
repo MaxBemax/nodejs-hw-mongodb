04f12d4b58b3f45db6e9fcc7a9b161af72f36e44
Max Bemax <7510332@gmail.com>
2025-09-19 20:56:53 +0300

    hw2: structure of project

---
+++ src/db/models/contacts.js
@@ -0,0 +1,22 @@
+import { model, Schema } from 'mongoose';
+
+const contactsSchema = new Schema(
+  {
+    name: { type: String, required: true },
+    phoneNumber: { type: String, required: true },
+    email: { type: String, required: true },
+    isFavourite: { type: Boolean, required: true },
+    contactType: {
+      type: String,
+      enum: ['work', 'home', 'personal'],
+      required: true,
+      default: 'personal',
+    },
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+  },
+);
+
+export const ContactsCollection = model('contacts', contactsSchema);
