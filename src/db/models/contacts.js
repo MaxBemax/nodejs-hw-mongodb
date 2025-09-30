76c63bb4230f8e07008a3b55359ec9cc77490371
Max Bemax <7510332@gmail.com>
2025-09-30 12:37:14 +0300

    hw3: changes

--- src/db/models/contacts.js
+++ src/db/models/contacts.js
@@ -4,8 +4,8 @@ const contactsSchema = new Schema(
   {
     name: { type: String, required: true },
     phoneNumber: { type: String, required: true },
-    email: { type: String, required: true },
-    isFavourite: { type: Boolean, required: true },
+    email: { type: String },
+    isFavourite: { type: Boolean, default: false },
     contactType: {
       type: String,
       enum: ['work', 'home', 'personal'],
---
+++ src/middlewares/errorHandler.js
@@ -0,0 +1,16 @@
+export const errorHandler = (err, req, res, next) => {
+  const status = err.status || 500;
+
+  if (status === 404) {
+    return res.status(404).json({
+      status: 404,
+      message: err.message || 'Contact not found',
+    });
+  }
+
+  res.status(status).json({
+    status,
+    message: err.message || 'Something went wrong',
+    data: err.message,
+  });
+};
