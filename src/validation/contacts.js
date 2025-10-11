4a3444635c60b4867ae1f1c87ed2bcb5c09ae945
Max Bemax <7510332@gmail.com>
2025-10-11 10:33:45 +0300

    add tips in validation

--- src/validation/contacts.js
+++ src/validation/contacts.js
@@ -1,19 +1,65 @@
 import Joi from 'joi';
 
 export const createContactSchema = Joi.object({
-  name: Joi.string().min(3).max(20).required(),
-  phoneNumber: Joi.string().min(3).max(20).required(),
-  email: Joi.string().email().min(3).max(20),
-  isFavourite: Joi.boolean(),
-  contactType: Joi.string().valid('work', 'home', 'personal').required(),
+  name: Joi.string().min(3).max(20).required().messages({
+    'string.base': 'Username should be a string',
+    'string.min': 'Username should have at least {#limit} characters',
+    'string.max': 'Username should have at most {#limit} characters',
+    'any.required': 'Username is required',
+    'any.empty': 'name cannot be empty',
+  }),
+
+  phoneNumber: Joi.string().min(3).max(20).required().messages({
+    'string.base': 'phoneNumber should be a string',
+    'string.min': 'phoneNumber should have at least {#limit} characters',
+    'string.max': 'phoneNumber should have at most {#limit} characters',
+    'any.required': 'phoneNumber is required',
+    'any.empty': 'name cannot be empty',
+  }),
+  email: Joi.string().email().min(3).max(20).messages({
+    'string.base': 'email should be a string',
+    'string.email': 'email must be a valid email address',
+    'string.min': 'email should have at least {#limit} characters',
+    'string.max': 'email should have at most {#limit} characters',
+  }),
+  isFavourite: Joi.boolean().messages({
+    'boolean.base': 'isFavourite should be a boolean value (true or false)',
+  }),
+  contactType: Joi.string()
+    .valid('work', 'home', 'personal')
+    .required()
+    .messages({
+      'string.base': 'contactType should be a string',
+      'any.only': 'contactType must be one of [work, home, personal]',
+      'any.required': 'contactType is required',
+      'any.empty': 'contactType cannot be empty',
+    }),
   // min/max у цьому полі зайве, бо значення обмежено переліком вказаних констант(enum).
 });
 
 export const updateContactSchema = Joi.object({
-  name: Joi.string().min(3).max(20),
-  phoneNumber: Joi.string().min(3).max(20),
-  email: Joi.string().email().min(3).max(20),
-  isFavourite: Joi.boolean(),
-  contactType: Joi.string().valid('work', 'home', 'personal'),
+  name: Joi.string().min(3).max(20).messages({
+    'string.base': 'name should be a string',
+    'string.min': 'name should have at least {#limit} characters',
+    'string.max': 'name should have at most {#limit} characters',
+  }),
+  phoneNumber: Joi.string().min(3).max(20).messages({
+    'string.base': 'phoneNumber should be a string',
+    'string.min': 'phoneNumber should have at least {#limit} characters',
+    'string.max': 'phoneNumber should have at most {#limit} characters',
+  }),
+  email: Joi.string().email().min(3).max(20).messages({
+    'string.base': 'email should be a string',
+    'string.email': 'email must be a valid email address',
+    'string.min': 'email should have at least {#limit} characters',
+    'string.max': 'email should have at most {#limit} characters',
+  }),
+  isFavourite: Joi.boolean().messages({
+    'boolean.base': 'isFavourite should be a boolean value (true or false)',
+  }),
+  contactType: Joi.string().valid('work', 'home', 'personal').messages({
+    'string.base': 'contactType should be a string',
+    'any.only': 'contactType must be one of [work, home, personal]',
+  }),
   // min/max у цьому полі зайве, бо значення обмежено переліком вказаних констант(enum).
 });
